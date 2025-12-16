afd0f5c595b75efeb8af8911a8fbdec1a683a5db
Oscar20001 <oscaralexander2000@gmail.com>
2025-12-16 12:01:33 -0600

    Fix TypeScript error in VentasView and RentasView

--- web/components/RentasView.tsx
+++ web/components/RentasView.tsx
@@ -39,7 +39,7 @@ export default function RentasView({ properties }: RentasViewProps) {
         ) : (
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
             {properties.map((property) => (
-              <PropertyCard key={property._id} property={property} />
+              <PropertyCard key={property._id as string} property={property} />
             ))}
           </div>
         )}
